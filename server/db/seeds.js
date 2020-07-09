3ab67d445e0a02fce7cbaf81d9f4b3df176b7069
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-07-09 20:47:32 +0100

    post functions finished

---
+++ server/db/seeds.js
@@ -0,0 +1,125 @@
+use dmc;
+db.dropDatabase();
+
+db.threads.insertMany([
+  {
+    code: 1,
+    colour: "White Tin",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 2,
+    colour: "Tin",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 3,
+    colour: "Tin",
+    shade: "Medium",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 4,
+    colour: "Tin",
+    shade: "Dark",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 5,
+    colour: "Driftwood",
+    shade: "Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 6,
+    colour: "Driftwood",
+    shade: "Medium Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 7,
+    colour: "Driftwood",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 8,
+    colour: "Driftwood",
+    shade: "Dark",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 9,
+    colour: "Cocoa",
+    shade: "Very Dark",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 10,
+    colour: "Tender Green",
+    shade: "Very Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 11,
+    colour: "Tender Green",
+    shade: "Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 12,
+    colour: "Tender Green",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 13,
+    colour: "Nile Green",
+    shade: "Medium Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 14,
+    colour: "Apple Green",
+    shade: "Pale",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 15,
+    colour: "Apple Green",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 16,
+    colour: "Chartreuse",
+    shade: "Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 17,
+    colour: "Yellow Plum",
+    shade: "Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 18,
+    colour: "Yellow Plum",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 19,
+    colour: "Autumn Gold",
+    shade: "Medium Light",
+    collection: "Stranded Cotton"
+  },
+  {
+    code: 20,
+    colour: "Shrimp",
+    shade: "N/A",
+    collection: "Stranded Cotton"
+  }
+])
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('dmc');
+    const threadsCollection = db.collection('threads');
+    const threadsRouter = createRouter(threadsCollection);
+  })
+  .catch(console.err);
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000')
+})
